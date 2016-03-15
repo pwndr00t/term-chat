d89e72b91fdec4667f3fd43ec816ee1542b55f9e
buraktekin <tknbrk@gmail.com>
2016-03-15 12:22:42 +0200

    Now adding new line available on a new message received during the current user writing sth.

--- term_chat.py
+++ term_chat.py
@@ -4,7 +4,7 @@
 import thread
 import logging
 from socketIO_client import SocketIO, LoggingNamespace
-import sys 
+import sys
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -12,19 +12,31 @@ sys.setdefaultencoding('utf8')
 logging.getLogger('requests').setLevel(logging.WARNING)
 logging.basicConfig(level=logging.ERROR)
 
+data = ""
+
 def on_response(*args):
     if type(args[0]) != str:
+        data = True
         print "\033[92m\033[1m" + args[0]["name"] + "\033[0m:", args[0]["text"]
+    else:
+        data = False
 
 with SocketIO('https://mede-chat-app.herokuapp.com/socket.io/', 443, LoggingNamespace) as socketIO:
+    user = raw_input("Enter your chat name please: ")
     socketIO.emit('joinRoom', {
-        "name": "Melih",
+        "name": user,
         "room": "deneme"
     })
     socketIO.on('message', on_response)
     thread.start_new_thread(socketIO.wait, ())
     while True:
-        msg = raw_input() + "\n"
-        socketIO.emit('message', {"name": "Melih", "text": msg})
-        print "\033[A                             \033[A"
-
+        msg = raw_input()
+        #TODO: @Melih This if is necessary or can we solve this from server side? We need sth. like
+        # When user on state 'typing' and a new message reached, then put received message first
+        # and regenerate raw_input box with previous state.
+        if data:
+            print msg, raw_input()
+        else:
+            print ""
+            socketIO.emit('message', {"name": "\033[92m\033[1m\n" + user, "text": msg})
+            print "\033[A                             \033[A"
